e913c7ae930d335349db1e6ef3ea1d002cdb1b55
App Academy Student <appacademy@Apps-Mac-mini-87.local>
2016-01-26 17:49:37 -0500

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+example = User.create!(username: 'exampleuser', email: 'fake@example.com', password: 'password')
+
+guest = User.create!(username: 'guest', email: 'guest@fake.com', password: 'password')
+
+guest.questions.create!(title: "Database config for Rails Heroku deployment?", body: "Can't get my database.yml to play nice with Heroku, any ideas?")
+
+example.questions.create!(title: "How do you answer questions on QuackOverflow?", body: "Trying to figure out how to answer on QuackOverflow?!?!?")
