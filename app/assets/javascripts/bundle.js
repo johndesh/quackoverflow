30cc1567d4df0fbe177a0ef3ed2a7e1b245460ad
App Academy Student <appacademy@Apps-Mac-mini-51.local>
2016-01-27 12:31:21 -0500

    add question show component

--- app/assets/javascripts/bundle.js
+++ app/assets/javascripts/bundle.js
@@ -60,12 +60,67 @@
 	};
 
 	var App = React.createClass({
-	  displayName: 'App'
+	  displayName: 'App',
+
+	  render: function () {
+	    return React.createElement(
+	      'div',
+	      { className: 'main' },
+	      React.createElement(
+	        'div',
+	        { className: 'question-index-wrapper' },
+	        React.createElement(
+	          'div',
+	          { className: 'sub-header' },
+	          React.createElement(
+	            'h2',
+	            { className: 'group' },
+	            'Top Questions'
+	          ),
+	          React.createElement(
+	            'nav',
+	            { className: 'sub-header-nav group' },
+	            React.createElement(
+	              'a',
+	              { href: '#', onClick: _handleClick, className: 'clicked group' },
+	              'interesting'
+	            ),
+	            React.createElement(
+	              'a',
+	              { href: '#', onClick: _handleClick, className: 'group' },
+	              'featured',
+	              React.createElement(
+	                'span',
+	                { className: 'featured-count-tab group' },
+	                '391'
+	              )
+	            ),
+	            React.createElement(
+	              'a',
+	              { href: '#', onClick: _handleClick, className: 'group' },
+	              'hot'
+	            ),
+	            React.createElement(
+	              'a',
+	              { href: '#', onClick: _handleClick, className: 'group' },
+	              'week'
+	            ),
+	            React.createElement(
+	              'a',
+	              { href: '#', onClick: _handleClick, className: 'group' },
+	              'month'
+	            )
+	          )
+	        ),
+	        this.props.children
+	      )
+	    );
+	  }
 	});
 
 	var routes = React.createElement(
 	  Route,
-	  { path: '/', component: QuestionsIndex },
+	  { path: '/', component: App },
 	  React.createElement(IndexRoute, { component: QuestionsIndex }),
 	  React.createElement(Route, { path: 'questions/:questionId', component: QuestionShow })
 	);
