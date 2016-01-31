591e4d65c94a0d0fb6e514a3aa9e84fa21ea2ec6
John Eshleman <johndesh@gmail.com>
2016-01-31 03:13:07 -0500

    fix user controller/application controller create bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  helper_method :current_user, :logged_in?
 
   def current_user
     @current_user ||= User.find_by_session_token(session[:token])
   end
 
-  def logged_in
+  def logged_in?
     !!current_user
   end
 
