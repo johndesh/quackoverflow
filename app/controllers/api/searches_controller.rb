51e3b2d1aff3d63a76ba85b5d690e5d6ff2d02bc
John Eshleman <johndesh@gmail.com>
2016-01-31 18:58:25 -0500

    fix n+1 query on search

--- app/controllers/api/searches_controller.rb
+++ app/controllers/api/searches_controller.rb
@@ -2,7 +2,7 @@ class Api::SearchesController < ApplicationController
 
 	def index
 		@search_results = PgSearch
-			.multisearch(params[:query])
+			.multisearch(params[:query]).includes(:searchable)
 	end
 
 end
