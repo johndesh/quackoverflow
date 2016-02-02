f8237999eac72460ebcc9fa0ecaf397a21faeb57
John Eshleman <johndesh@gmail.com>
2016-02-02 12:24:39 -0500

    spinner

--- frontend/components/spinner.jsx
+++ frontend/components/spinner.jsx
@@ -2,7 +2,38 @@ var React, cx, objectAssign;
 
 React = require('react');
 
-cx = require('classnames');
+/*!
+  Copyright (c) 2015 Jed Watson.
+  Licensed under the MIT License (MIT), see
+  http://jedwatson.github.io/classnames
+*/
+
+cx = function classNames() {
+	var classes = '';
+	var arg;
+
+	for (var i = 0; i < arguments.length; i++) {
+		arg = arguments[i];
+		if (!arg) {
+			continue;
+		}
+
+		if ('string' === typeof arg || 'number' === typeof arg) {
+			classes += ' ' + arg;
+		} else if (Object.prototype.toString.call(arg) === '[object Array]') {
+			classes += ' ' + classNames.apply(null, arg);
+		} else if ('object' === typeof arg) {
+			for (var key in arg) {
+				if (!arg.hasOwnProperty(key) || !arg[key]) {
+					continue;
+				}
+				classes += ' ' + key;
+			}
+		}
+	}
+	return classes.substr(1);
+};
+
 
 objectAssign = require('react/lib/Object.assign');
 
